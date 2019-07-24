6787e9150e606631679d0042aa3dc01d469f60c9
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-24 20:28:32 +0200

    feat: change authDomain to aplikacja.ameryka.com.pl

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,6 @@
 export const FIREBASE_CONFIG = {
     apiKey: "AIzaSyDkFr0-ugr_Zw37jp2C20hmN-5TCWcsCsk",
-    authDomain: "amerykahospital-advice-68385.firebaseapp.com",
+    authDomain: "aplikacja.ameryka.com.pl",
     databaseURL: "https://amerykahospital-advice-68385.firebaseio.com",
     projectId: "amerykahospital-advice-68385",
     storageBucket: "amerykahospital-advice-68385.appspot.com",
