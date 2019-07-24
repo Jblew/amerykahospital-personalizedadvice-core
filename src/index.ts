979cdd9f8aa8d350e81d9eb12a3b454a2c4b032f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-24 10:46:01 +0200

    refactor: move configuration to config dir

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { AdviceIdGenerator } from "./AdviceIdGenerator";
-export { FirestoreCollections } from "./FirestoreCollections";
-export { FirebaseFunctionDefinitions } from "./FirebaseFunctionDefinitions";
-export { FIREBASE_CONFIG } from "./firebase.config";
+export { FirestoreCollections } from "./config/FirestoreCollections";
+export { FirebaseFunctionDefinitions } from "./config/FirebaseFunctionDefinitions";
+export { FIREBASE_CONFIG } from "./config/firebase.config";
 export { Advice } from "./model/Advice";
 export { AdvicesManager } from "./orm/AdvicesManager";
