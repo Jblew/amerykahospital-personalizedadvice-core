4a67f00cd8a4c8481784fb369907342084c60bee
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-25 14:33:16 +0200

    feat(functions): add definition for ImportAdviceToUser function

--- src/config/FirebaseFunctionDefinitions.ts
+++ src/config/FirebaseFunctionDefinitions.ts
@@ -1,4 +1,4 @@
-import { PendingAdvice } from "../model/PendingAdvice";
+import { Advice, PendingAdvice } from "../model/PendingAdvice";
 
 export namespace FirebaseFunctionDefinitions {
     export namespace AddAdvice {
@@ -30,5 +30,21 @@ export namespace FirebaseFunctionDefinitions {
         }
 
         export const NAME = "send_sms";
+    }
+
+    export namespace ImportAdviceToUser {
+        export type Function = (data: Input, context: any) => Promise<Result>;
+        export type Adapter = (data: Input) => Promise<Result>;
+
+        export interface AdviceId {
+            adviceId: string;
+        }
+        export type Input = AdviceId;
+
+        export interface Result {
+            advice: Advice;
+        }
+
+        export const NAME = "import_advice_to_user";
     }
 }
