94f206fb9ba5a320d6ab2ff6a715df029bba3188
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-08-12 20:04:30 +0200

    fix: remove unused MEDICALPROFESSIONAL_UIDS_COLLECTION collection key
    BREAKING CHANGE  remove unused MEDICALPROFESSIONAL_UIDS_COLLECTION collection key

--- src/config/FirestoreCollections.ts
+++ src/config/FirestoreCollections.ts
@@ -1,5 +1,4 @@
 export class FirestoreCollections {
-    public static MEDICALPROFESSIONAL_UIDS_COLLECTION = "/ahpaa_medicalprofessional_uids";
     public static ADVICES_COLLECTION_KEY = "ahpaa_advices";
     public static SENT_SMS_MESSAGES_COLLECTION_KEY = "sent_sms_messages";
 }
